bda1564255972e7a1b9c1b2b457af253256a7ca4
Abdalrahman_Gamal <abdalrahmangamal200@gmail.com>
2026-03-05 04:38:15 +0200

    feat(protectRoutes): protect routes is done by checking if user is authenticated or not

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,5 +1,32 @@
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const FullPage = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 function ProtectedRoute({ children }) {
-  return children;
+  const { isLoading, isAuthenticated } = useUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated) navigate("/login");
+  }, [isAuthenticated, isLoading, navigate]);
+
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  if (isAuthenticated) return children;
 }
 
 export default ProtectedRoute;
